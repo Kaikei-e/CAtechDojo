176db9478fb5341d5638580ddf07d545b73c1ab5
kaikei <kaikei-e@protonmail.com>
2021-07-16 22:23:06 +0900

    Add: init gin.

--- go/goAPIs/main.go
+++ go/goAPIs/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main(){
-	router := gin
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"test": "hello world",
+		})
+	})
+
+	router.Run(":8085")
 }
