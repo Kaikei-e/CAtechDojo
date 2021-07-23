2085d411d747836c9c5c88fd115c67f1f6b9d10d
Kaikei-e <e_kaikei@protonmail.com>
2021-07-23 19:24:38 +0900

    Update: defining tables.

--- mysql/init.sql
+++ mysql/init.sql
@@ -4,3 +4,21 @@ Use CAtech;
 
 
 Create table CAtech.users(id int not null primary key AUTO_INCREMENT, name varchar(20), token varchar(200));
+
+CREATE TABLE `CAtech`.`gacha_table` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `chara_name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
+
+  CREATE TABLE `CAtech`.`userPossession` (
+  `uniqueID` CHAR(100) NOT NULL,
+  `chara_ID` INT NOT NULL,
+  `chara_name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`uniqueID`));
+
+  CREATE TABLE `CAtech`.`gacha_rate` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `chara_id` INT NOT NULL,
+  `chara_rarity` INT NOT NULL DEFAULT 0,
+  `weight` INT NOT NULL,
+  PRIMARY KEY (`id`));
